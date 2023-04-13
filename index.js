834c360300282c53350fc773e7a7040dab96e378
olgakahraman <ol.khrmn@gmail.com>
2023-04-12 20:41:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 //document.getElementById("count-el").innerText=6
 
 
-let saveEl = document.getElementById("save-el");
-let countEl = document.getElementById("count-el");
+let countEl = document.getElementById("count-el"); //h2 numbers
+let saveEl = document.getElementById("save-el");   // p previous entries
 let count = 0;
 
 console.log(saveEl);
 
 function increment() {
-  count += 1 ;
+  count += 1;
   countEl.textContent = count;
 }
 
 function save() {
-let message = count + " - ";
-saveEl.textContent += message;  
+  let message = count + " - ";
+  saveEl.textContent += message;
 
   countEl.textContent = 0;
   count = 0;
